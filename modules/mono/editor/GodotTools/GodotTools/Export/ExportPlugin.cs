69df13417705cb0502cff6f7114e031a6041e45a
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-06-07 13:24:24 +0200

    Allow non-self-contained `dotnet publish`

--- modules/mono/editor/GodotTools/GodotTools/Export/ExportPlugin.cs
+++ modules/mono/editor/GodotTools/GodotTools/Export/ExportPlugin.cs
@@ -71,6 +71,17 @@ namespace GodotTools.Export
                         }
                     },
                     { "default_value", false }
+                },
+                new Godot.Collections.Dictionary()
+                {
+                    {
+                        "option", new Godot.Collections.Dictionary()
+                        {
+                            { "name", "dotnet/self_contained" },
+                            { "type", (int)Variant.Type.Bool }
+                        }
+                    },
+                    { "default_value", true }
                 }
             };
         }
@@ -256,7 +267,7 @@ namespace GodotTools.Export
 
                     // Execute dotnet publish.
                     if (!BuildManager.PublishProjectBlocking(buildConfig, platform,
-                            runtimeIdentifier, publishOutputDir, includeDebugSymbols))
+                            runtimeIdentifier, publishOutputDir, includeDebugSymbols, (bool)GetOption("dotnet/self_contained")))
                     {
                         throw new InvalidOperationException("Failed to build project.");
                     }
